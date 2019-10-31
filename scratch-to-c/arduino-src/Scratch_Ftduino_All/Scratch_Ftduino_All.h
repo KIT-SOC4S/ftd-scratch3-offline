ad0243005a1e44f5b08aff42e664eadb0e25c31c
intrigus <abc123zeus@live.de>
2019-11-01 00:37:00 +0100

    Move support files to special directory

---
+++ scratch-to-c/arduino-src/Scratch_Ftduino_All/Scratch_Ftduino_All.h
@@ -0,0 +1,9 @@
+#ifndef SCRATCH_FTDUINO_ALL_H
+#define SCRATCH_FTDUINO_ALL_H
+
+#include "scratch.h"
+#include "cast.h"
+#include "operatorsh.h"
+#include "scratch_ftduino.h"
+
+#endif /* SCRATCH_FTDUINO_ALL_H */
--- scratch-to-c/arduino-src/arduino-src.ino
+++ scratch-to-c/arduino-src/arduino-src.ino
@@ -1,7 +1,14 @@
+#define FTDUINO_NO_SHORTHANDS
+#include <Ftduino.h>
+#undef FTDUINO_NO_SHORTHANDS
+#include <Scratch_Ftduino_All.h>
 
 
 void setup() {
+  ftduino.init();
+  pinMode(LED_BUILTIN, OUTPUT);
 }
+
 void loop() {
-  
+ 
 }
